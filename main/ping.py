7ea390b182410e6a96a8d0d6dd604574352c228e
JamaL <jamalikowow@mail.ru>
2026-04-01 23:24:53 +0300

    подключение Sentry

--- main/ping.py
+++ main/ping.py
@@ -1,4 +1,15 @@
+import os
 from fastapi import FastAPI, HTTPException
+import sentry_sdk
+from sentry_sdk.integrations.fastapi import FastApiIntegration
+
+# Инициализация Sentry
+sentry_sdk.init(
+    dsn=os.getenv("SENTRY_DSN"),
+    integrations=[FastApiIntegration()],
+    traces_sample_rate=1.0,  # Для трейсинга запросов (опционально)
+    send_default_pii=True,  # Отправлять ли данные пользователей (IP, заголовки)
+)
 
 app = FastAPI()
 
